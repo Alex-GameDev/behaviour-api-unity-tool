88b47a0898685bdb57f9ac8314044cd9e4538f39
AlejandroOrejudo2000 <alexgamedev2000@gmail.com>
2022-12-16 15:10:40 +0100

    FEAT - Custom graph drawer

---
+++ Assets/BehaviourAPI Unity Tool/Editor/Attributes/CustomGraphDrawerAttribute.cs
@@ -0,0 +1,22 @@
+using BehaviourAPI.Core;
+using System;
+
+namespace BehaviourAPI.Unity.Editor
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class CustomGraphDrawerAttribute : Attribute
+    {
+        public Type GraphType;
+        public CustomGraphDrawerAttribute(Type graphType)
+        {
+            if(graphType.IsSubclassOf(typeof(BehaviourGraph)))
+            {
+                GraphType = graphType;
+            }
+            else
+            {
+                throw new ArgumentException("Error: type argument must be a subclass of BehaviourGraph");
+            }
+        }
+    }
+}
