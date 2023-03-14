bb893ee6c21db588d69a4692f750a1af8d6df4d1
AlejandroOrejudo2000 <alexgamedev2000@gmail.com>
2023-03-14 17:24:49 +0100

    REF - Unity curve factor

--- Assets/BehaviourAPI Unity Package/Behaviour API/Extensions/Nodes/UnityCurveFactor.cs
+++ Assets/BehaviourAPI Unity Package/Behaviour API/Extensions/Nodes/UnityCurveFactor.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 namespace BehaviourAPI.UnityExtensions
 {
     using UtilitySystems;
-    public class CurveFunction : CurveFactor
+
+    /// <summary>
+    /// Create the function using unity animation curve
+    /// </summary>
+    public class UnityCurveFactor : CurveFactor
     {
         public AnimationCurve curve;
 
         protected override float Evaluate(float childUtility)
         {
             return curve.Evaluate(childUtility);
-        }  
-        
-        public CurveFunction SetCurve(AnimationCurve animationCurve)
+        }
+
+        public UnityCurveFactor SetCurve(AnimationCurve animationCurve)
         {
             curve = animationCurve;
             return this;
